a11544ffe91320080df2a2365696973c579fb24a
takanome_dev <takanomedev221@gmail.com>
2022-12-25 09:22:49 +0000

    feat: add tailwind

---
+++ apps/web/pages/_app.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../global.css';
+
+import type { AppProps } from 'next/app';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp;
