2f0717784ac3b017fff0a83e61175398ae36e37d
takanome_dev <takanomedev221@gmail.com>
2023-05-01 11:22:42 +0000

    update nextjs to the latest

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,7 +1,21 @@
 // tailwind config is required for editor support
-const sharedConfig = require("tailwind-config/tailwind.config.js");
+const sharedConfig = require('tailwind-config/tailwind.config.js');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   presets: [sharedConfig],
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+    },
+  },
 };
