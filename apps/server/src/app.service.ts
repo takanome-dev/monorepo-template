7bd65db317ade11fb23af6e3958f9ff070c2dff5
takanome_dev <takanomedev221@gmail.com>
2023-06-20 09:00:27 +0000

    fix: ci build

--- apps/server/src/app.service.ts
+++ apps/server/src/app.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  // eslint-disable-next-line class-methods-use-this
   getHello() {
     return { message: 'Hello from the NestJS server 👋' };
   }
